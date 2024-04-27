237cc1464597d5a4c41243e3b1aec8ed12359817
Rahul Mahato <mahatorahul2901@gmail.com>
2024-04-27 15:23:07 +0530

    updating mx-12 to the existing pull request

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -18,7 +18,7 @@ const Appbar = () => {
           <Link to="/publish">
             <button
               type="button"
-              className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+              className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mx-12"
             >
               Write
             </button>
