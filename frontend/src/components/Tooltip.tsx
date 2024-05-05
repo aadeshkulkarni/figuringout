fe0974b22ed865b511dc2ff9542f1c24967b5f20
James Okada <jyo1291@gmail.com>
2024-05-05 00:00:19 -0700

    fix build

--- frontend/src/components/Tooltip.tsx
+++ frontend/src/components/Tooltip.tsx
@@ -1,11 +1,13 @@
+import { ReactNode } from "react";
+
 interface TooltipProps {
   message: string;
-  children?: () => void;
+  children?: ReactNode;
 }
 const Tooltip = ({ message, children }: TooltipProps) => {
   return (
-    <div class="group relative flex">
-      <span class="absolute bottom-full bg-white z-50 scale-0 transition-all rounded p-2 text-s text-black group-hover:scale-100 border">
+    <div className="group relative flex">
+      <span className="absolute bottom-full bg-white z-50 scale-0 transition-all rounded p-2 text-s text-black group-hover:scale-100 border">
         {message}
       </span>
       {children}
