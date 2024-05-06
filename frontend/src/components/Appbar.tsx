96ff118595e910a068e9098b882ab9b709bb0c71
vijaynvvr <vijayvardhansn10@gmail.com>
2024-05-06 22:30:56 +0530

    changed the signin button label according to the specification

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -30,7 +30,7 @@ const Appbar = () => {
         to="/signin"
         className="focus:outline-none text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mx-12"
         >
-          Signin
+          Sign In
         </Link>
       )}
     </div>
