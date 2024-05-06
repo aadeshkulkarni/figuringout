a8e229e0ebfa303bcbb9b3b7c248e0f0a237e34c
vijaynvvr <vijayvardhansn10@gmail.com>
2024-05-06 22:29:21 +0530

    added signin button in appbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -13,7 +13,7 @@ const Appbar = () => {
       <Link to="/blogs" className="text-xl font-bold">
         Medium
       </Link>
-      {isUserLoggedIn && (
+      {isUserLoggedIn ? (
         <div className="flex gap-4 md:gap-8">
           <Link to="/publish">
             <button
@@ -25,6 +25,13 @@ const Appbar = () => {
           </Link>
           <ProfileBox />
         </div>
+      ) : (
+        <Link
+        to="/signin"
+        className="focus:outline-none text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mx-12"
+        >
+          Signin
+        </Link>
       )}
     </div>
   );
