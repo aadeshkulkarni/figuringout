52fe20cc9a7dd1b5be897db32f34d2529aa69120
Krupa Panchal <krupapanchal.dev@gmail.com>
2024-05-14 14:52:00 +0530

    Feat: Make blogs page public
    [Issue#35](https://github.com/aadeshkulkarni/medium-app/issues/35)

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
 
 const Hero = () => {
-  const token = localStorage.getItem("token");
   return (
     <div className="w-screen h-[500px] bg-yellow-500 flex flex-col justify-center items-center">
       <h1 className="text-4xl md:text-7xl tracking-wide font-extrabold text-center md:text-left">Stay Curious.</h1>
       <h6 className="text-2xl md:text-4xl tracking-wide font-extralight text-center md:text-left py-6">Discover stories, thinking, and expertise from writers on any topic.</h6>
-      <Link to={`${token ? "/blogs" : "/signup"}`}>
+      <Link to="/blogs">
         <button className="py-4 px-8 rounded-full bg-gray-800 hover:bg-black text-lg tracking-wider text-white shadow-lg shadow-yellow-600 hover:shadow-sm">Start reading</button>
       </Link>
     </div>
