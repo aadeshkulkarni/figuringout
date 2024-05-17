ca50231fa0a66b5187db54656d181693ee164b23
dhairya-create <dhairyaparikh769@gmail.com>
2024-05-17 14:24:38 +0530

    Fixed bug #50: Added Loader in button in Publish Post Component

--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -11,7 +11,7 @@ import "react-quill/dist/quill.snow.css";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ToastWrapper from "../components/ToastWrapper";
-
+import Spinner from "../components/Spinner";
 const Publish = () => {
   const navigate = useNavigate();
   const [title, setTitle] = useState("");
@@ -69,30 +69,7 @@ const Publish = () => {
           className={`w-[150px] items-center px-5 py-2.5 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-800 ${loading ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-700'}`}
           disabled={loading}
         >
-          {loading ? (
-            <svg
-              className="animate-spin h-5 w-5 text-white mx-auto"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8v8H4z"
-              ></path>
-            </svg>
-          ) : (
-            "Publish post"
-          )}
+         {loading ? <Spinner /> : "Publish post"}
         </button>
       </div>
       <ToastWrapper />
