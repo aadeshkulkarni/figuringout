7df6a3ac3ef69b14d3c6b1a10cd81491de0a7e33
Aadesh Kulkarni <aadeshkulkarni@Aadeshs-MacBook-Pro.local>
2024-06-02 03:30:34 +0530

    name changed to figuring out

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -20,8 +20,8 @@ const Appbar = ({ skipAuthCheck = false, pageActions, hideWriteAction = false }:
   return (
     <div className="border-b border-slate-100 flex justify-between items-center p-4 md:px-16">
       <div className="flex justify-center items-center gap-4">
-        <Link to="/" className="text-xl font-bold">
-          Medium
+        <Link to="/" className="text-xl font-light">
+        <span className="text-gray-700">figuring</span><span className="text-black">out</span><span className="text-red-500">.Life</span>
         </Link>
         <Search />
       </div>
