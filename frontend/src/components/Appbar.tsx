fb642d491abe97883039fb094b432e0795132e54
Intercotrading <kunals@intercotradingco.com>
2024-06-03 13:22:19 -0500

    UI Inconsistency Fix

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -18,7 +18,7 @@ const Appbar = ({ skipAuthCheck = false, pageActions, hideWriteAction = false }:
     navigate('/signin');
   }
   return (
-    <div className="border-b border-slate-100 flex justify-between items-center p-4 md:px-16">
+    <div className="border-b border-slate-100 flex justify-between items-center p-4 md:px-16 md:flex-wrap">
       <div className="flex justify-center items-center gap-4">
         <Link to="/" className="text-xl font-light">
         <span className="text-gray-700">figuring</span><span className="text-black">out</span><span className="text-green-700">.Life</span>
