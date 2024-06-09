6fb7e9352ff6bb9075b319ba6e3e0c6c0e397b9f
Aadesh Kulkarni <aadeshkulkarni@Aadeshs-MacBook-Pro.local>
2024-06-09 23:45:40 +0530

    design changes for mobile screen

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -20,8 +20,8 @@ const Appbar = ({ skipAuthCheck = false, pageActions, hideWriteAction = false }:
   return (
     <div className="border-b border-slate-100 flex justify-between items-center p-4 md:px-16 md:flex-wrap">
       <div className="flex justify-center items-center gap-4">
-        <Link to="/" className="text-xl font-light">
-        <span className="text-gray-700">figuring</span><span className="text-black">out</span><span className="text-green-700">.Life</span>
+        <Link to="/" className="text-xl font-medium">
+        <span className="text-gray-700">Figuring</span><span className="text-black">out</span><span className="text-green-700">.Life</span>
         </Link>
         <Search />
       </div>
--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -25,34 +25,32 @@ const BlogCard = ({ author, title, content, publishedDate, id, fullWidth, tagsOn
   return (
     <Link
       to={`/blog/${id}`}
-      className={`blog-card px-4 py-8 ${fullWidth ? 'w-full' : 'w-full md:w-3/6'} md:border-b md:border-gray-200 my-2 bg-white shadow-b-sm grid grid-cols-12 md:gap-6 lg:gap-0`}
+      className={`blog-card px-4 py-8 ${fullWidth ? 'w-full' : 'w-full md:w-3/6'} md:border-b md:border-gray-200 my-2 bg-white shadow-b-sm flex flex-col md:grid md:grid-cols-12 md:gap-6 lg:gap-0`}
     >
-      <div className="col-span-12 md:col-span-9 md:px-4">
-        <div className="flex items-center gap-4">
+      <div className="order-2 flex flex-col md:order-none md:col-span-9 py-4 md:px-4">
+        <div className="order-3 md:order-none flex items-center gap-4">
           <Avatar name={author?.name || ''} imageSrc={author?.profilePic} />
           <div>
-            <span>{author?.name}</span> · <span className="text-sm text-slate-500">{formatDateString(publishedDate)}</span>
+            <span>{author?.name}</span> ·{' '}
+            <span className="text-sm text-slate-500">{formatDateString(publishedDate)}</span>
           </div>
         </div>
-        <div className="text-xl font-bold pt-4">{title}</div>
-        <div className="tracking-wide py-4 text-slate-600">
+        <div className="order-1 md:order-none text-xl font-bold pt-4">{title}</div>
+        <div className="order-2 md:order-none tracking-wide py-4 text-slate-600">
           <ReactQuill value={quillContent} readOnly={true} theme={'bubble'} />
         </div>
       </div>
-      <div className="hidden col-span-0 md:col-span-3 p-4 md:flex justify-center items-center">
+      <div className="order-1 md:order-none col-span-0 md:col-span-3 md:p-4 flex justify-center items-center">
         <ArticleImage uniqueId={id} />
       </div>
-      <div className="flex col-span-full md:px-2">
-        <div className='flex'>
-					  {tagsOnPost?.slice(0,2).map((tagWrapper) => {
-						  return (
-							  <Pill id={tagWrapper.tag.id} tagName={tagWrapper.tag.tagName}/>
-						  )
-					})}
-				</div>
-        <div className="text-gray-600 pt-4">{Math.ceil(content.length / 300)} min read</div>
-
+      <div className="order-3 flex col-span-full border-b md:border-none border-gray-100 pb-8 md:px-2 pb-0">
+        <div className="flex">
+          {tagsOnPost?.slice(0, 2).map((tagWrapper) => {
+            return <Pill id={tagWrapper.tag.id} tagName={tagWrapper.tag.tagName} />;
+          })}
         </div>
+        <div className="order-3 md:order-none text-gray-600 pt-4">{Math.ceil(content.length / 300)} min read</div>
+      </div>
     </Link>
   );
 };
@@ -61,7 +59,7 @@ export default BlogCard;
 
 function ArticleImage({ uniqueId }: { uniqueId: string }) {
   return (
-    <object data={`https://source.unsplash.com/random/150x150?sig=${uniqueId}`} type="image/jpeg">
+    <object data={`https://picsum.photos/300/300?random=${uniqueId}`} type="image/jpeg" className="w-full">
       <div className="bg-gray-50 w-[100%] animate-pulse aspect-square"></div>
     </object>
   );
