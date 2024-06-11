96e79a206c802376355a000c7593d151f86d6b7e
Jaishree2310 <jaishrees23102001@gmail.com>
2024-06-11 15:19:27 +0530

    Instead of showing the white screen, inform the user that they have not done any activity. And also display a write blog button.

--- frontend/src/components/User/UserProfile.tsx
+++ frontend/src/components/User/UserProfile.tsx
@@ -28,7 +28,7 @@ const NoBlogsMessage = () => (
     <p className="text-lg text-gray-600 mb-3">You haven't written any blogs yet.</p>
     <Link to="/publish">
     <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className="bg-green-900 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
       onClick={() => console.log('Navigate to write blog')}
     >
       Write your first blog
