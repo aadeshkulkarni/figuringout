6f4627d8fce7a9320ff07d9d537b4bec1221f898
AtharvaChobe <103590543+AtharvaChobe@users.noreply.github.com>
2024-06-12 14:13:01 +0530

    route changed for logged in user

--- frontend/src/components/BookmarkTab.tsx
+++ frontend/src/components/BookmarkTab.tsx
@@ -6,10 +6,15 @@ type BookmarkTabProps = {
   bookmarks: Post[];
 };
 const BookmarkTab = ({ bookmarks }: BookmarkTabProps) => {
+
+  const user = localStorage.getItem("user");
+
+  
   return (
     <div className="p-4 md:p-10 ">
       <h1 className="text-2xl font-bold mb-2">Bookmarks</h1>
       <div className="flex flex-col justify-center items-center bg-gray-50">
+
         {bookmarks.length > 0 &&
           bookmarks.map((bookmark) => (
             <BlogCard
@@ -23,7 +28,8 @@ const BookmarkTab = ({ bookmarks }: BookmarkTabProps) => {
             />
           ))}
       </div>
-      {bookmarks.length === 0 && (
+
+      {bookmarks.length === 0 && !user && (
         <div className="flex justify-center mt-10">
           <h6 className="text-xl font-bold py-2">
             No bookmarks yet! You can bookmark blogs in the blogs page{' '}
@@ -33,6 +39,19 @@ const BookmarkTab = ({ bookmarks }: BookmarkTabProps) => {
           </h6>
         </div>
       )}
+
+
+      {bookmarks.length === 0 && user && (
+        <div className="flex justify-center mt-10">
+          <h6 className="text-xl font-bold py-2">
+            No bookmarks yet! You can bookmark blogs in the blogs page{' '}
+            <Link to="/blogs" className="underline">
+              here
+            </Link>
+          </h6>
+        </div>
+      )}
+
     </div>
   );
 };
