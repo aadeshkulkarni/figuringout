2a291bb2aa639a37f5699a0978ec2aa206ed9fad
Aadesh Kulkarni <aadeshkulkarni@Aadeshs-MacBook-Pro.local>
2024-06-15 13:08:47 +0530

    added topics and contributors list

--- frontend/src/pages/Contributor.tsx
+++ frontend/src/pages/Contributor.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Appbar from '../components/Appbar';
-import GitHubIcon from '../components/icons/Github';
 import { GITHUB_CONTRIBUTOR_URL } from '../config';
 import { SkeletonContributorGrid } from '../skeletons/SkeletonContributor';
 import { Link } from 'react-router-dom';
+interface ContributorProp {
+  id: string;
+  html_url: string;
+  avatar_url: string;
+  login: string;
+  contributions: string;
+}
 
 const Contributors: React.FC = () => {
   const [contributors, setContributors] = useState<any[]>([]);
@@ -40,38 +46,51 @@ const Contributors: React.FC = () => {
   return (
     <>
       <Appbar skipAuthCheck={true} />
+      <ContributionNotification />
       <div className="p-4 flex flex-col justify-center items-center w-screen">
-        <h1 className="text-2xl mb-4 text-center">Contributors</h1>
-        <div className="md:w-4/5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {contributors.map((contributor: any) => (
-            <a
-              key={contributor?.id}
-              href={contributor?.html_url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-white p-4 flex flex-col items-center justify-center transform transition-transform duration-300 hover:scale-105 border border-gray-100"
-            >
-              <div className="bg-gray-200 w-full h-full flex items-center justify-center mb-4 overflow-hidden">
-                <img src={contributor?.avatar_url} alt={contributor?.login} className="w-full h-full object-cover" />
-              </div>
-
-              <div className="text-center">
-                <div className="px-20">
-                  <GitHubIcon />
-                </div>
-
-                <h2 className="text-lg font-medium">{contributor.login}</h2>
-                <p className="text-gray-600">{contributor?.contributions} contributions</p>
-              </div>
-            </a>
-          ))}
-        </div>
+        <h1 className="text-2xl mb-4">Contributors</h1>
+        <ContributorsList contributors={contributors} />
       </div>
-      <div className="bg-gray-50 border text-gray-700 w-full p-4 text-center">
-          <div className="py-4">If you would like to contribute to the project, you can do it by clicking on this link.</div>
-          <Link className="px-4 py-2 bg-black text-white rounded-full text-xs" to="https://github.com/aadeshkulkarni/medium-app">Contribute</Link>
-        </div>
     </>
+  );
+};
+
+const ContributionNotification = () => (
+  <div className="bg-gray-50 border text-gray-700 w-full p-4 text-center">
+    <div className="py-4">If you would like to contribute to the project, you can do it by clicking on this link.</div>
+    <Link
+      className="px-4 py-2 bg-black text-white rounded-full text-xs"
+      to="https://github.com/aadeshkulkarni/medium-app"
+    >
+      Contribute
+    </Link>
+  </div>
+);
+
+const ContributorsList = ({ contributors }: { contributors: ContributorProp[] }) => (
+  <div className="md:w-3/5 grid grid-cols-12 gap-4">
+    {contributors.map((contributor: any) => (
+      <ContributorCard contributor={contributor} />
+    ))}
+  </div>
+);
+
+const ContributorCard = ({ contributor }: { contributor: ContributorProp }) => {
+  return (
+    <a
+      key={contributor?.id}
+      href={contributor?.html_url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="bg-white flex flex-col col-span-12 md:col-span-3 transform transition-transform duration-300 hover:scale-105 border shadow-sm"
+    >
+      <img src={contributor?.avatar_url} alt={contributor?.login} className="w-full  object-cover" />
+      <div className="text-left p-2">
+        <h2 className="text-lg font-medium">{contributor.login}</h2>
+        <h2>Developer</h2>
+        <p className="text-gray-600">{contributor?.contributions} contributions</p>
+      </div>
+    </a>
   );
 };
 
