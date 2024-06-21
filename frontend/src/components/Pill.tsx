705784e9bc04c20a620d254b903ae206afab00ad
bruno-corso <brunotcorso@gmail.com>
2024-06-20 23:12:30 -0300

    ajuste issue #196 Bug | Blog | Topic click

--- frontend/src/components/Pill.tsx
+++ frontend/src/components/Pill.tsx
@@ -1,13 +1,17 @@
+import { Link } from 'react-router-dom';
+
 interface PillProps {
   id: string;
   tagName: string;
 }
 export function Pill({ id, tagName }: PillProps) {
   return (
-    <div className="p-2" key={id}>
-      <div className="bg-gray-50 border border-gray-300 text-gray-500 hover:text-gray-600 py-2 px-4 rounded-3xl flex items-center justify-center text-xs cursor-pointer">
-        {tagName}
+    <Link to={`/blogs?tag=${id}`}>
+      <div className="p-2" key={id}>
+        <div className="bg-gray-50 border border-gray-300 text-gray-500 hover:text-gray-600 py-2 px-4 rounded-3xl flex items-center justify-center text-xs cursor-pointer">
+          {tagName}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- frontend/src/components/Tags.tsx
+++ frontend/src/components/Tags.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useBlog } from '../hooks';
 import { Pill } from './Pill';
 
