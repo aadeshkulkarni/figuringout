88bb0e8f1ab0e88f84db2fdbf1cc1d9ec16c6052
Dev <dev2005suthar@gmail.com>
2024-07-01 17:43:22 +0530

    fixed string conversion in api url

--- frontend/src/store/atoms/singleblog.ts
+++ frontend/src/store/atoms/singleblog.ts
@@ -7,7 +7,7 @@ export const blogAtom = atomFamily({
     default:selectorFamily({
         key:"blogSelector",
         get:(id)=>async()=>{
-            const res = await axios.get(`${BACKEND_URL}/api/v1/blog/${id}`,{
+            const res = await axios.get(`${BACKEND_URL}/api/v1/blog/${String(id)}`,{
                 headers:{
                     "Authorization":`Bearer ${localStorage.getItem("token")}`
                 }
