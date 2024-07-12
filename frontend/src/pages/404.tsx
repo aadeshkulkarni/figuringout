6c1674112bba51674ae141ab49480a5ae7ce4251
0xprathamesh <prathameshpatil6499@gmail.com>
2024-07-12 12:58:19 +0530

    Enhanced 404 page for mobile view ports

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -6,11 +6,11 @@ const Custom404 = () => {
     <>
       <Appbar skipAuthCheck />
 
-      <div className="m-52 flex flex-col justify-center items-center ">
-        <h1 className="text-4xl font-bold text-gray-500 dark:text-gray-200">404 - Not Found</h1>
-        <p className="text-gray-600 mt-4 tracking-wide text-sub">The page you're looking for could not be found.</p>
-        <Link className="p-4" to={'/'}>
-          <button className="bg-gray-50 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 text-gray-500 dark:text-gray-300 hover:text-gray-600 dark:hover:text-gray-100  py-2 px-6 rounded-3xl flex items-center justify-center text-xs cursor-pointer ">
+      <div className="flex flex-col justify-center items-center py-52 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-500 dark:text-gray-200 text-center">404 - Not Found</h1>
+        <p className="text-gray-600 mt-4 tracking-wide text-center text-sm sm:text-base md:text-lg">The page you're looking for could not be found.</p>
+        <Link className="mt-6" to={'/'}>
+          <button className="bg-gray-50 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 text-gray-500 dark:text-gray-300 hover:text-gray-600 dark:hover:text-gray-100 py-2 px-6 rounded-3xl text-xs sm:text-sm md:text-base">
             Go back home
           </button>
         </Link>
