eee43e9714a3bece97ab7ff108d8105146bdc2cf
Chirag Gowda <chiragfrost876@gmail.com>
2024-07-14 10:36:58 +0530

    Fixed few issues

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,5 @@
-export const BACKEND_URL = 'https://backend.aadesh-kulkarni08.workers.dev';
+// export const BACKEND_URL = 'https://backend.aadesh-kulkarni08.workers.dev';
+export const BACKEND_URL = 'http://localhost:39013';
 export const GITHUB_CONTRIBUTOR_URL = 'https://api.github.com/repos/aadeshkulkarni/medium-app/contributors';
 // export const BACKEND_URL = "http://localhost:8787";
 export const FF_ENABLE_AI = true; // Feature flag (temp solution)
