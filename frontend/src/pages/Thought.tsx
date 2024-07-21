b3db8afaa1f75a839486da299927e742f948ac9a
Chirag Gowda <chiragfrost876@gmail.com>
2024-07-21 13:23:36 +0530

    Fixed frontend build errors

--- frontend/src/pages/Thought.tsx
+++ frontend/src/pages/Thought.tsx
@@ -1,11 +1,13 @@
 import Appbar from '../components/Appbar';
 import { BACKEND_URL } from '../config';
 import { createThoughtInput, updateThoughtInput } from '@aadeshk/medium-common';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export const Thought = ()=>{
     const [title, setTitle] = useState<string | null>(null)
     const [content, setContent] = useState<string | null>(null)
-    const {fetchedThoughts, setFetchedThoughts} = useState<{id : string, title:string , content : string} | null>(null)
+    const [fetchedThoughts, setFetchedThoughts] = useState<{id : string, content : string}[] | null>(null)
     return (
         <div>
             <Appbar skipAuthCheck />
@@ -17,7 +19,7 @@ export const Thought = ()=>{
                     </div>
                     <div>
                         <button onChange = {async ()=>{
-                            const res = axios.post(`${BACKEND_URL}/api/v1/thought/`, {
+                            const res = await axios.post(`${BACKEND_URL}/api/v1/thought/`, {
                                 content,
                                 title
                             })
@@ -25,19 +27,19 @@ export const Thought = ()=>{
                     </div>
                     <div>
                         <button onClick = {async ()=>{
-                            const thoughts = axios.get(`${BACKEND_URL}/api/v1/thought/bulk`)
+                            const thoughts = await axios.get(`${BACKEND_URL}/api/v1/thought/bulk`)
                             setFetchedThoughts(thoughts.data)
                         }}>Show all thoughts</button>
                     </div>
                     <div>
                         <button onClick = {async ()=>{
-                            const res = axios.delete(`${BACKEND_URL}/api/v1/thought/paste_id_here`)
+                            const res = await axios.delete(`${BACKEND_URL}/api/v1/thought/paste_id_here`)
                             
                         }}>Delete Thought(Paste id in axios url to delete particular)</button>
                     </div>
                     <div>
                         <button onClick = {async ()=>{
-                            const res = axios.put(`${BACKEND_URL}/api/v1/thought/`, {
+                            const res = await axios.put(`${BACKEND_URL}/api/v1/thought/`, {
                                 content,
                                 title
                             })
@@ -45,9 +47,9 @@ export const Thought = ()=>{
                         }}>Update</button>
                     </div>
                     <div>
-                        {fetchedThoughts.map((t)=>{
+                        {fetchedThoughts?.map((t : any)=>{
                             return <div className = "py-3">
-                                <div>{t.title}</div>
+                                {/* <div>{t.title}</div> */}
                                 <div>{t.content}</div>
                             </div>
                         })}
