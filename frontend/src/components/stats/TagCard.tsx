d4d3408df1c218367bb56be20dd6d065d45bd5b9
nipunh <nipunhedaoo@gmail.com>
2024-07-22 18:00:26 -0400

    Update TagCard.tsx

--- frontend/src/components/stats/TagCard.tsx
+++ frontend/src/components/stats/TagCard.tsx
@@ -4,9 +4,6 @@ const TagCard = ({ tagList }: { tagList: [] | undefined }) => {
       <div className="flex flex-col h-28 col-span-12 md:col-span-3 transform transition-transform duration-300 hover:scale-105 border rounded-md shadow-sm text-main bg-main">
         <div className="text-left p-4">
           <h4 className="text-lg font-medium text-nowrap overflow-hidden text-ellipsis">Top Tags</h4>
-          <div className="flex justify-center max-w-fit items-center m-1 font-medium py-1 px-2 bg-transparent rounded-full text-whi bg-gray-100 border">
-            <div className="text-xs font-normal leading-none max-w-full flex-initial">Hello</div>
-          </div>
           {tagList?.map((tags, index) => {
             return (
               <div
