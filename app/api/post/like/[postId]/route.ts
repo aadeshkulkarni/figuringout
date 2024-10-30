8d741ec4e9536b69ced63498aa43e1ec9dcad461
Aadesh Kulkarni <aadeshkulkarni@Aadeshs-MacBook-Pro.local>
2024-10-30 19:51:46 +0530

    fixed build error

--- app/api/post/like/[postId]/route.ts
+++ app/api/post/like/[postId]/route.ts
@@ -3,9 +3,10 @@ import Post from "@/models/Post";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-// TODO: Refactor Error handling 
-export const GET = async (request: Request, { params }: { params: { postId: string } }) => {
-  const postId = await params.postId;
+// TODO: Refactor Error handling
+export const GET = async (request: Request, { params }: { params: Promise<{ slug: string }> }) => {
+  // @ts-ignore
+  const postId = (await params).postId;
   if (!postId) return NextResponse.json({ message: "PostId missing" });
   const session = await getServerSession(NEXT_AUTH);
   if (!session) return NextResponse.json({ message: "Not Authenticated" });
@@ -14,7 +15,9 @@ export const GET = async (request: Request, { params }: { params: { postId: stri
   if (!post) {
     return NextResponse.json({ message: "Post not found" });
   }
-  const alreadyLiked = post.likes.some((like: { userId: { equals: (arg0: any) => any; }; }) => like.userId.equals(userId));
+  const alreadyLiked = post.likes.some((like: { userId: { equals: (arg0: any) => any } }) =>
+    like.userId.equals(userId)
+  );
   if (!alreadyLiked) {
     post.likes.push({ userId, timestamp: new Date() });
     await post.save();
